f9e08fa202b5d61cb3047689af09d48330195392
PineappleBox <37129442+PineappleBox@users.noreply.github.com>
2019-01-23 21:31:12 -0500

    Update install.php

--- install.php
+++ install.php
@@ -1,6 +1,7 @@
 <?php
-$installer = file_get_contents(/php-install.txt');
+$installer = file_get_contents('/php-install.txt');
 $installer .= file_get_contents('/html-install.txt');
 $file = file_put_contents('installer.php',$installer);
 unlink(basename(__FILE__));
 header('Location: installer.php');
+?>
